4731b3ab94df0eb471023eea5678ba3b0a916ee3
aguscastaneda <castaneda.agustin.et21.21@gmail.com>
2026-04-14 12:40:01 -0300

    Implement list modification functions
    Implemented functions to replace empty strings with a given value and to remove occurrences of a specified value from a list.

--- list_modify.py
+++ list_modify.py
@@ -12,8 +12,13 @@ def put(value, lst):
         put("Blue", colors) -> 1
         # colors ahora es ["Red", "Blue", "Green"]
     """
-    return "ANSWER HERE"  # Remove this line and implement
-
+    
+    for i in range(len(lst)):
+        if lst[i] == "":
+            lst[i] = value
+            return i
+    return -1
+       
 
 def remove(value, lst):
     """
@@ -26,4 +31,9 @@ def remove(value, lst):
         remove("Red", colors) -> 2
         # colors ahora es ["", "Green", "", "Blue"]
     """
-    return "ANSWER HERE"  # Remove this line and implement
+    counter = 0
+    for i in range(len(lst)):
+        if lst[i] == value:
+            lst[i] = ""
+            counter += 1
+    return counter
