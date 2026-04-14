66caa0a63d1def33575a2b52e15ecd4b21231d39
aguscastaneda <castaneda.agustin.et21.21@gmail.com>
2026-04-14 12:39:03 -0300

    Update powers.py

--- powers.py
+++ powers.py
@@ -7,7 +7,14 @@ def power(base, exp):
 
     Ejemplo: power(2, 3) -> 8  (2*2*2)
     """
-    return "ANSWER HERE"  # Remove this line and implement
+
+    if exp >= 0:
+        res = 1
+        for i in range(exp):
+            res = res * base
+        return res
+    else:
+        return 1
 
 
 def sum_of_powers(base, max_exp):
@@ -17,4 +24,7 @@ def sum_of_powers(base, max_exp):
 
     Ejemplo: sum_of_powers(2, 3) -> 15  (1+2+4+8)
     """
-    return "ANSWER HERE"  # Remove this line and implement
+    res = 0
+    for i in range(max_exp + 1):
+        res += base ** i
+    return res
